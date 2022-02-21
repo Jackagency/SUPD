6ee15399169eac99bf108bc0d9bcb96740f68267
andreev_e_v <Diablo372@yandex.ru>
2022-02-21 17:59:40 +0300

    added randoms utils and started groups autotests

--- src/test/java/tests/TestGroups.java
+++ src/test/java/tests/TestGroups.java
@@ -1,4 +1,44 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pages.UserPageComponents;
+
+import static com.codeborne.selenide.Selenide.*;
+
 public class TestGroups {
+
+    UserPageComponents userPageComponents = new UserPageComponents();
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "http://192.168.151.19:8080/";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @AfterEach
+    public void clearCache() {
+        closeWebDriver();
+    }
+
+    @Test
+    @DisplayName("Group creation")
+    public void groupCreate(){
+        open("login");
+        userPageComponents.authorizeSupd("admin", "123");
+
+        $("div.v-btn-toggle a:nth-child(2) span").click();
+
+        $("div.groups__actions:nth-child(1) span").click();
+
+
+        sleep(5000);
+
+
+
+    }
 }
